cb1cecc8916ccdd4afbf9c4871d485690ac064e4
Jan Ole Suhr <ole.suhr@rocket-es.com>
2019-05-01 18:14:33 +0200

    Changing the directory structure ...

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -9,7 +9,7 @@ class PackageServiceProvider extends ServiceProvider
     public function register()
     {
         // $configPath = __DIR__.'/../config/demo.php';
-        // $this->mergeConfigFrom($configPath, 'lcs');
+        // $this->mergeConfigFrom($configPath, 'demo');
     }
 
     public function boot()
@@ -18,7 +18,7 @@ class PackageServiceProvider extends ServiceProvider
         $this->commands([\janole\Demo\Console\Commands\DemoCommand::class]);
 
         // Register some demo routes
-        $this->loadRoutesFrom(__DIR__.'/../src/routes/routes.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
 
         // Register some demo views
         // (use them with the corresponding prefix like: @include("demo-views::the-view-name"))
